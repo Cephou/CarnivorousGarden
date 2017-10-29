dc4e5d9c067c499bb1f19a0c78c967aaf5024709
Cephou <laurent.noyon@gmail.com>
2017-10-29 23:28:22 +0100

    test de persistence

--- src/java/Application/MainCtrl.java
+++ src/java/Application/MainCtrl.java
@@ -6,23 +6,48 @@
 package Application;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.faces.bean.ViewScoped;
 
-/**
- *
- * @author Laurent
- */
 @Named(value = "mainCtrl")
 @ViewScoped
 
 public class MainCtrl implements Serializable {
 
-    //@EJB
-    //private PlantDAO plantDAO;
+    @EJB
+    private PlantDAO plantDAO;
+    
+    private Plant newPlant;
 
     public MainCtrl() {
+        newPlant = new Plant();
+    }
+    
+    public List<Plant> getPlants() {
+        return plantDAO.allPlants();
+    }
+    
+    public void addPlant() {
+        plantDAO.add(this.newPlant);
+        this.newPlant = new Plant();
+    }
+    
+    public PlantDAO getPlantDAO() {
+        return plantDAO;
+    }
+
+    public void setPlantDAO(PlantDAO plantDAO) {
+        this.plantDAO = plantDAO;
+    }
+
+    public Plant getNewPlant() {
+        return newPlant;
+    }
+
+    public void setNewPlant(Plant newPlant) {
+        this.newPlant = newPlant;
     }
     
 }
---
+++ src/java/Application/PlantDAO.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Application;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Laurent
+ */
+@Stateless
+public class PlantDAO {
+    
+    @PersistenceContext(unitName = "CarnivorousGardenPU")
+    private EntityManager em;
+    
+    public List<Plant> allPlants() {
+        Query query = em.createNamedQuery("Plant.findAll");
+        return query.getResultList();
+    }
+    
+    public void add(Plant p) {
+        em.persist(p);
+        em.flush();
+    }
+    
+    public void edit(Plant p) {
+        em.merge(p);
+        em.flush();
+    }
+
+    public void remove(Plant p) {
+        em.remove(em.merge(p));
+        em.flush();
+    }
+}
+
